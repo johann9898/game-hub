587d352505fb30e2bea635d2679bae84a3ba72b4
Jóhann Rúnarsson <johann9898@gmail.com>
2023-06-21 00:29:54 +0000

    Create a custom hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "223a597e38324328a1582ad5f31f73cb"
+        key: "1432785e82b64a9ab16337777b21720f"
     }
 });
