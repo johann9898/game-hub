a315819804eef09ae5c880dd553b58e5316797db
Jóhann Rúnarsson <johann9898@gmail.com>
2023-06-27 10:31:36 +0000

    fix infitite query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
-const apiClient = new APIClient<Platform>("/platform/lists/parents");
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
     id: number;
@@ -14,8 +14,7 @@ const usePlatforms = () => useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24hr
-    initialData: { count: platforms.length, results: platforms }
-
+    initialData: { count: platforms.length, next: null, results: platforms }
 });
 
 export default usePlatforms;
